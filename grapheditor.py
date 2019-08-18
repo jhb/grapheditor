aaeb7011e34564b7a5ca1dee9b738b5408f2aab7
Joerg Baach <mail@baach.de>
2019-08-18 11:52:06 +0200

    First working editing of nodes

--- grapheditor.py
+++ grapheditor.py
@@ -6,6 +6,7 @@ import flask
 from flask import request, url_for, redirect
 from neo4j import GraphDatabase
 from flask_socketio import SocketIO
+from werkzeug.datastructures import MultiDict
 
 api = flask.Flask(__name__)
 api.secret_key = b'GraphIsGreat'
@@ -63,21 +64,32 @@ def getNode(nid):
         return result.single()['n']
 
 
-def updateNode(nid, items):
+def updateNode(node, items):
+    nid = node.id
     itemsd = dict(items)
     print(itemsd)
     t = "n.%s = '%s'"
     parts = []
     for k, v in items:
-        if k.startswith('new_'):
-            continue
-        if k.startswith('labels'):
+        if k.startswith('new_') or k.startswith('labels') or k=='nid':
             continue
         parts.append(t % (k, v))
-    if itemsd['new_name']:
+    if itemsd.get('new_name',''):
         parts.append(t % (itemsd['new_name'], itemsd['new_value']))
-    statement = """MATCH (n) WHERE id(n) = %s SET %s""" % (nid, ', '.join(parts))
-    statement += ", n:%s" % itemsd['labels']
+    statement = "MATCH (n) WHERE id(n) = %s " % nid
+
+    oldlabels = set(node.labels)
+    newlabels = set([l.strip() for l in itemsd['labels'].split(':')])
+    toremove = oldlabels.difference(newlabels)
+    toadd = newlabels.difference(oldlabels)
+
+    if toremove:
+        statement+='REMOVE n:%s ' % ':'.join(toremove)
+    statement+="SET %s " % ', '.join(parts)
+    if toadd:
+        if parts:
+            statement+=', '
+        statement += "n:%s" % itemsd['labels']
     print(statement)
     with driver.session() as session:
         result = session.run(statement)
@@ -91,8 +103,9 @@ def delNodeProperty(nid, propertyname):
 
 
 @api.route('/node/<nid>', methods=['GET', 'POST'])
-def node(nid=7):
+def node(nid=7,req=None):
     node = getNode(int(nid))
+
 
     if 'delete' in request.args:
         delNodeProperty(nid, request.args['delete'])
@@ -110,7 +123,7 @@ def node(nid=7):
     else:
         items = node.items()
         items = list(items)
-        items.append(('labels', ':'.join(node.labels)))
+        items.append(('labels', ':'.join(sorted(node.labels))))
 
     print(items)
 
@@ -146,26 +159,55 @@ def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    nodes = getNodes()
-    msg = {'template':getTemplate('nodelist')(nodes=getNodes())}
-    socketio.emit('my response', msg, callback=messageReceived)
-
 @socketio.on('node clicked')
 def handle_node_clicked(json,methods=['GET','POST']):
     print('node clicked',json)
 
+def newnode(nid=7,formdata=None):
+    node = getNode(int(nid))
+    if 'delete' in request.args:
+        delNodeProperty(nid, request.args['delete'])
+        flask.flash('%s removed from node' % request.args['delete'])
+        return flask.redirect(request.base_url)
 
+    class MyForm(Form):
+        pass
+
+    MyForm.labels = StringField('labels', description='labels of the node')
+
+    if formdata:
+        items = formdata.items()
+        items = list(items)
+    else:
+        items = node.items()
+        items = list(items)
+        items.append(('labels', ':'.join(sorted(node.labels))))
+
+    print(items)
+
+    for k, v in sorted(items):
+        setattr(MyForm, k, StringField(k, description='foo description'))
+    MyForm.new_name = StringField('name', description='The name of the property')
+    MyForm.new_value = StringField('value', description='The value of the new property')
+
+    form = MyForm(formdata, DictObject(items))
+
+    if formdata and form.validate():
+        statement = updateNode(node, items)
+        #flask.flash('Node %s updated <small> -- %s</small>' % (nid, statement))
+        #return flask.redirect('/node/%s' % nid)
+
+    template = getTemplate('nodeform.pt')
+
+    return template(form=form, node=node)
 
 
 
 @socketio.on('gee')
-def dispatch_ge_event(json,methods=['GET','POST']):
-    print('gee',json)
-    for func in eventroutes.get(json['event'],[]):
-        print(func)
-        out = func(json)
+def dispatch_ge_event(msg,methods=['GET','POST']):
+    print('gee',msg)
+    for func in eventroutes.get(msg['event'],[]):
+        out = func(msg)
         if type(out)!=type([]):
             out=[out]
         for msg in out:
@@ -175,6 +217,7 @@ def shownodelist(msg):
     nodes = getNodes()
     msg = {'event':   'display',
            'section': 'graph',
+           'occ': 'nodelist',
            'html':    getTemplate('nodelist')(nodes=getNodes())}
     return msg
 
@@ -183,27 +226,41 @@ def shownodeview(msg):
     nid = int(msg['nid'])
     msg = {'event':   'display',
            'section': 'view',
+           'occ': nid,
            'html':    getTemplate('nodeview')(node=getNode(nid))}
     return msg
 
-def shownodeform(msg):
-    nid = int(msg['nid'])
+def shownodeform(msg,nid=None):
+    if not nid:
+        nid = int(msg['nid'])
     msg = {'event': 'display',
            'section': 'action',
-           'html': node(nid)}
+           'occ': nid,
+           'html': newnode(nid)}
     return msg
 
-def nodecommit(msg):
-    nid =int(msg['nid'])
+def nodesubmit(msg):
+    nid = int(msg['formdata']['nid'])
+    newnode(nid,MultiDict(msg['formdata'].items()))
+    out=[shownodeform(msg,nid)]
+    # newmsg = {'event':   'clear',
+    #        'section': 'action'};
+    # out = [newmsg]
+    out.append(shownodelist(msg))
+    if nid == msg['occupied']['view']:
+        msg['nid']=nid
+        out.append(shownodeview(msg))
+    return out
 
 def emit(msg):
     socketio.emit('gee', msg, callback=messageReceived)
 
 eventroutes = {'init':[shownodelist],
                'node clicked':[shownodeview],
+               'node hover': [shownodeview],
                'node edit': [shownodeform],
-               'node commit': [nodecommit]}
+               'node submit': [nodesubmit]}
 
 if __name__ == '__main__':
     print('x' * 30)
-    socketio.run(api, debug=1, port=9000)
+    socketio.run(api, debug=1, port=9000, )
